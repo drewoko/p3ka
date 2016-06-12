8166b277a70e6010e8de80a984a62f1b0c5af11f
Deniss Gubanov <deniss@gubanov.ee>
2016-06-12 09:47:05 +0300

    Database struct refactor Changed SQLite library to go-sqlite3

--- database.go
+++ database.go
@@ -3,18 +3,23 @@ package main
 import (
 	"time"
 
-	"github.com/mxk/go-sqlite/sqlite3"
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
+const MESSAGE_MAIN_QUERY string = "SELECT id, name, origurl as url, mature FROM messages";
+
+type RowMap map[string]interface{}
+
 type DataBase struct {
-	db *sqlite3.Conn
+	db *sql.DB
 }
 
 func (self *DataBase) init(path string) *DataBase {
 
 	var err error
-
-	self.db, err = sqlite3.Open(path)
+	self.db, err = sql.Open("sqlite3", path)
 
 	if(err != nil) {
 		log.Fatal("Failed to open database. Reason: ", err)
@@ -27,152 +32,109 @@ func (self *DataBase) init(path string) *DataBase {
 
 func (self *DataBase) create_table() {
 
-	self.db.Exec("CREATE TABLE messages (id integer not null primary key, date int, messageid int, name text, origurl text, deleted int DEFAULT 0)")
-	err := self.db.Exec("ALTER TABLE messages ADD COLUMN mature int DEFAULT 0;")
-
-	log.Info("", err)
+	self.db.Query("CREATE TABLE messages (id integer not null primary key, date int, messageid int, name text, origurl text, deleted int DEFAULT 0)")
+	self.db.Query("ALTER TABLE messages ADD COLUMN mature int DEFAULT 0;")
 }
 
 func (self *DataBase) is_exists(author string, url string) bool {
-
-	for s, err := self.db.Query("SELECT * FROM messages WHERE origurl=?", url); err == nil; err = s.Next() {
-		return true
-	}
-
-	return false
+	s := self.db.QueryRow("SELECT COUNT(id) as cnt FROM messages WHERE origurl=?", url)
+	var cnt int
+	s.Scan(&cnt)
+	return cnt > 0
 }
 
 func (self *DataBase) add_row(messageid int64, author string, url string, mature bool) {
 
-	self.db.Begin()
-	err := self.db.Exec(
+	_, err := self.db.Exec(
 		"INSERT INTO messages (messageid, date, name, origurl, mature) VALUES (?, ?, ?, ?, ?)",
 			int(messageid), int(time.Now().Unix()), author, url, mature)
-
-	self.db.Commit()
 
 	if(err != nil) {
 		self.process_error(err)
 	}
 }
 
-func (self *DataBase) get_message_by_id(id int64) sqlite3.RowMap {
-
-	row := make(sqlite3.RowMap)
-	for s, err := self.db.Query("SELECT * FROM messages where deleted = 0 and id=?", int(id)); err == nil; err = s.Next() {
-		s.Scan(row)
-	}
-
-	return row
+func (self *DataBase) get_message_by_id(id int64) RowMap {
+	s := self.db.QueryRow(MESSAGE_MAIN_QUERY + " where deleted = 0 and id=?", int(id));
+	return self.single_message_scan(s)
 }
 
-func (self *DataBase) get_message_by_messageid(id int64) sqlite3.RowMap {
-
-	row := make(sqlite3.RowMap)
-	for s, err := self.db.Query("SELECT * FROM messages where deleted = 0 and messageid=?", int(id)); err == nil; err = s.Next() {
-		s.Scan(row)
-	}
-
-	return row
+func (self *DataBase) get_message_by_messageid(id int64) RowMap {
+	s := self.db.QueryRow(MESSAGE_MAIN_QUERY + " where deleted = 0 and messageid=?", int(id))
+	return self.single_message_scan(s)
 }
 
 func (self *DataBase) set_deleted(id interface{}) {
-	self.db.Exec("UPDATE messages set deleted = 1 where id=?", id)
+	self.db.Query("UPDATE messages set deleted = 1 where id=?", id)
 }
 
-func (self *DataBase) process_error(err error) {
-	log.Info("Failed to execute query. Reason: ", err)
-}
+func (self *DataBase) get_all() []RowMap {
+	s, err := self.db.Query(MESSAGE_MAIN_QUERY + " where deleted=0");
 
-func (self *DataBase) get_all_outdated() []sqlite3.RowMap {
-
-	last_time := int(time.Now().Unix())-15552000
-
-	var rows []sqlite3.RowMap
-	row := make(sqlite3.RowMap)
-
-	for s, err := self.db.Query("SELECT * FROM messages where deleted=0 and date < ?", last_time); err == nil; err = s.Next() {
-		s.Scan(row)
-
-		rows = append(rows, row)
+	if(err != nil) {
+		self.process_error(err)
 	}
 
-	return rows
+	return self.multiple_message_scan(s)
 }
 
+func (self *DataBase) get_last(limit int, start int) []RowMap {
 
-func (self *DataBase) get_all() []sqlite3.RowMap {
+	s, err := self.db.Query(DaDa
+		MESSAGE_MAIN_QUERY + " where deleted=0 order by id desc limit ?,?", start, limit)
 
-	var rows []sqlite3.RowMap
-
-	for s, err := self.db.Query("SELECT * FROM messages where deleted=0"); err == nil; err = s.Next() {
-		row := make(sqlite3.RowMap)
-		s.Scan(row)
-		rows = append(rows, row)
+	if(err != nil) {
+		self.process_error(err)
 	}
 
-	return rows
+	return self.multiple_message_scan(s)
 }
 
-func (self *DataBase) get_last(limit int, start int) []sqlite3.RowMap {
+func (self *DataBase) get_last_user(limit int, start int, user string) []RowMap {
 
-	var rows []sqlite3.RowMap
+	s, err := self.db.Query(
+		MESSAGE_MAIN_QUERY + " where deleted=0 and name=? order by id desc limit ?,?", user, start, limit)
 
-	for s, err := self.db.Query(
-		"SELECT name, origurl as url, mature FROM messages where deleted=0 order by id desc limit ?,?", start, limit); err == nil; err = s.Next() {
-		row := make(sqlite3.RowMap)
-
-		s.Scan(row)
-
-		rows = append(rows, row)
+	if(err != nil) {
+		self.process_error(err)
 	}
 
-	return rows
+	return self.multiple_message_scan(s)
 }
 
+func (self *DataBase) get_random(limit int) []RowMap {
 
-func (self *DataBase) get_last_user(limit int, start int, user string) []sqlite3.RowMap {
+	s, err := self.db.Query(
+		MESSAGE_MAIN_QUERY + " WHERE deleted = 0 ORDER BY RANDOM() LIMIT ?", limit)
 
-	var rows []sqlite3.RowMap
-
-	log.Info("GetAllUsers req. limit: %s, start: %s, user: %s", limit, start, user)
-
-	for s, err := self.db.Query(
-		"SELECT name, origurl as url, mature FROM messages where deleted=0 and name=? order by id desc limit ?,?", user, start, limit); err == nil; err = s.Next() {
-		row := make(sqlite3.RowMap)
-
-		s.Scan(row)
-
-		rows = append(rows, row)
+	if(err != nil) {
+		self.process_error(err)
 	}
 
-	return rows
+	return self.multiple_message_scan(s)
 }
 
-func (self *DataBase) get_random(limit int) []sqlite3.RowMap {
+func (self *DataBase) get_top_users(limit int, exclude []string) []RowMap {
 
-	var rows []sqlite3.RowMap
+	var rows []RowMap
 
-	for s, err := self.db.Query(
-		"SELECT name, origurl as url, mature FROM messages WHERE deleted = 0 ORDER BY RANDOM() LIMIT ?", limit); err == nil; err = s.Next() {
-		row := make(sqlite3.RowMap)
+	s, err := self.db.Query(
+		"select count(*) as cnt, name from messages where deleted = 0 group by name order by cnt desc LIMIT ?", limit)
 
-		s.Scan(row)
-
-		rows = append(rows, row)
+	if(err != nil) {
+		self.process_error(err)
 	}
 
-	return rows
-}
+	for s.Next() {
+		row := make(RowMap)
 
-func (self *DataBase) get_top_users(limit int, exclude []string) []sqlite3.RowMap {
+		var cnt int
+		var name string
 
-	var rows []sqlite3.RowMap
+		s.Scan(&cnt, &name)
 
-	for s, err := self.db.Query(
-		"select count(*) as cnt, name from messages where deleted = 0 group by name order by cnt desc LIMIT ?", limit); err == nil; err = s.Next() {
-		row := make(sqlite3.RowMap)
-		s.Scan(row)
+		row["cnt"] = cnt
+		row["name"] = name
 
 		if(!contains_string(exclude, row["name"].(string))) {
 			rows = append(rows, row)
@@ -180,4 +142,54 @@ func (self *DataBase) get_top_users(limit int, exclude []string) []sqlite3.RowMa
 	}
 
 	return rows
+}
+
+func (self *DataBase) multiple_message_scan(s *sql.Rows) []RowMap {
+	var rows []RowMap
+
+	for s.Next() {
+		rows = append(rows, self.multiple_item_message_scan(s))
+	}
+
+	return rows
+}
+
+func (self *DataBase) multiple_item_message_scan(s *sql.Rows) RowMap {
+	row := make(RowMap)
+
+	var id int
+	var name string
+	var url string
+	var mature int
+
+	s.Scan(&id, &name, &url, &mature)
+
+	row["id"] = id
+	row["name"] = name
+	row["url"] = url
+	row["mature"] = mature
+
+	return row
+}
+
+func (self *DataBase) single_message_scan(s *sql.Row) RowMap {
+	row := make(RowMap)
+
+	var id int
+	var name string
+	var url string
+	var mature int
+
+	s.Scan(&id, &name, &url, &mature)
+
+	row["id"] = id
+	row["name"] = name
+	row["url"] = url
+	row["mature"] = mature
+
+	return row
+}
+
+func (self *DataBase) process_error(err error) {
+	log.Info("Failed to execute query. Reason: ", err)
 }
